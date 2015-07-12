2d7832b61a1e7ed01a3892b636d18c480255fabf
Tobsta <oliver.balfour@actetd.com>
2015-07-12 11:26:24 +1000

    Create DBMS Table.js

---
+++ src/DBMS Table.js
@@ -0,0 +1,18 @@
+/* Tables */
+
+//function to create a table within a database
+DBMS.createTable = function(database, name, cols){
+	this.selectDatabase(database).tables.push({
+		name: name,
+		columns: cols,
+		rows: []
+	});
+	this.postAll();
+}
+
+//function to select a table
+DBMS.selectTable = function(database, table){
+	return this.selectProperty(table, this.selectDatabase(database).tables);
+}
+
+/* Needs more methods, deleteTable, deleteColumn, addColumn etc. */
